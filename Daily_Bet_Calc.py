625db34a95eea904ab0f25ccb97ec52eaffbecd1
Vespid <shadifox@gmail.com>
2017-07-24 20:08:43 -0700

    update for optimized food_tools

--- Daily_Bet_Calc.py
+++ Daily_Bet_Calc.py
@@ -8,38 +8,16 @@ risk=1
 max_bet=10100
 
 
+Arenas,est_prob,Payouts,OpenOdds=ft.get_todays_data()
+combos, Odds = ft.AORO_combos(Arenas, OpenOdds, Payouts)
+bets, TER=ft.max_TER_bets(combos, max_bet, risk)
 
-#####Calculate Today's Round#####
-#a,o,p=ft.get_todays_data()
-#combodf=ft.calc_combos(a,o,p)
-#bets,TER=ft.calc_bets(combodf, max_bet, risk)
-#ft.calc_bust(ft.calc_cumulative(bets, ft.calc_oddsdf(bets, o)),bets,rnd,risk)
-
-bets,TER,combos=ft.today_AORO_bets(max_bet, risk)
+print("TER: ",TER)
+OddsDF=ft.calc_oddsdf(bets, Odds)
+localdf=ft.calc_cumulative(bets, OddsDF)
+ft.calc_bust(localdf,bets,rnd,risk)
 
 
 ft.start()
 time.sleep(2)
 ft.daqtools(bets,max_bet)
-
-
-#import time
-#t1=time.time()
-######Calculate Past Round#####
-#ft.get_past_data(rnd)
-#a,o,p,w=ft.load_past_data(rnd)
-#combodf=ft.calc_combos(a,o,p)
-#
-#bets=ft.calc_bets(combodf, max_bet, risk)
-##
-#print(ft.calc_winnings(bets,w))
-##
-##
-#clfbets=ft.clf_bet(combodf, max_bet, risk)
-#print(ft.calc_winnings(clfbets,w))
-#ft.calc_bust(ft.calc_cumulative(clfbets, ft.calc_oddsdf(clfbets, o)),clfbets,rnd,risk)
-
-#zoso=ft.daq_bets(rnd, max_bet, risk)
-#a,o,p,w=ft.calc_odds(rnd)
-#combodf=ft.calc_combos(a,o,p)
-#bets=ft.calc_bets(combodf, max_bet, risk)
--- Reddit_Bet_Calc.py
+++ Reddit_Bet_Calc.py
@@ -1,52 +1,19 @@
 import food_tools as ft
 import warnings
 import math, time
-import pickle
 
 warnings.filterwarnings('ignore')
 
 rnd=math.floor((time.time()-926028865)/60/60/24)
-risk=0.95
-max_bet=1000
+risk=1
+max_bet=7500
 
 
 
 #####Calculate Today's Round#####
-a,o,p,oo=ft.get_todays_data()
-combodf=ft.calc_combos(a,o,p)
-bets,TER=ft.calc_bets(combodf, max_bet, risk)
+
+Arenas,est_prob,Payouts,OpenOdds=ft.get_todays_data()
+combos, Odds = ft.AORO_combos(Arenas, OpenOdds, Payouts)
+bets, TER=ft.max_TER_bets(combos, max_bet, risk)
+
 ft.reddit_format(rnd,bets)
-
-#ft.calc_bust(ft.calc_cumulative(bets, ft.calc_oddsdf(bets, o)),bets,rnd,risk)
-#ft.start()
-#ft.daqtools(bets,max_bet)
-
-
-#import time
-#t1=time.time()
-######Calculate Past Round#####
-#ft.get_past_data(rnd)
-#a,o,p,w=ft.load_past_data(rnd)
-#combodf=ft.calc_combos(a,o,p)
-#
-#bets=ft.calc_bets(combodf, max_bet, risk)
-##
-#print(ft.calc_winnings(bets,w))
-##
-##
-#clfbets=ft.clf_bet(combodf, max_bet, risk)
-#print(ft.calc_winnings(clfbets,w))
-#ft.calc_bust(ft.calc_cumulative(clfbets, ft.calc_oddsdf(clfbets, o)),clfbets,rnd,risk)
-
-#zoso=ft.daq_bets(rnd, max_bet, risk)
-#a,o,p,w=ft.calc_odds(rnd)
-#combodf=ft.calc_combos(a,o,p)
-#bets=ft.calc_bets(combodf, max_bet, risk)
-#
-#redditbet_file=open("MyRedditBets.pickle","rb")
-#reddit_bets = pickle.load(redditbet_file)
-#redditbet_file.close()
-#reddit_bets[rnd]=bets
-#redditbet_file=open("MyRedditBets.pickle","wb")
-#pickle.dump(reddit_bets, redditbet_file)
-#redditbet_file.close()
